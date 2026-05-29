2ac42be2128282b174187710c7154c29dd087e84
Daner Alejandro Salazar Colorado <daneralejandro03@gmail.com>
2026-05-29 15:53:57 -0500

    refactor: migrar a C++ con módulo de seguridad y PlatformIO

---
+++ Proyecto/include/Seguridad.h
@@ -0,0 +1,13 @@
+//
+// Created by salazar on 29/05/26.
+//
+
+#ifndef PROYECTO_SEGURIDAD_H
+#define PROYECTO_SEGURIDAD_H
+
+
+class Seguridad {
+};
+
+
+#endif //PROYECTO_SEGURIDAD_H
---
+++ Proyecto/src/Seguridad.cpp
@@ -0,0 +1,5 @@
+//
+// Created by salazar on 29/05/26.
+//
+
+#include "Seguridad.h"
